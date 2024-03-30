feea7eab269d5a9254caf9bae3700be3464d4bb3
josesn08 <josedelossantossuareznegrete@gmail.com>
2024-03-30 22:45:31 +0100

    Avances del 30-03-2024

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import {HomeComponent} from './pages/home/home.component';
+import {LabsComponent} from './pages/labs/labs.component';
+import { Component } from '@angular/core';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'labs',
+    component: LabsComponent
+  }
+
+];
